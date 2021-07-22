d3533042c3920d6aeaa943b85cbd8ff1088517b2
darbyjack <admin@glaremasters.me>
2021-07-22 01:18:03 -0500

    Go go go

--- src/main/java/io/github/holovid/server/command/StopCommand.java
+++ src/main/java/io/github/holovid/server/command/StopCommand.java
@@ -2,17 +2,9 @@ package io.github.holovid.server.command;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
 
-@ShellComponent
+
 public final class StopCommand {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StopCommand.class);
 
-    @ShellMethod("stop")
-    public void stop() {
-        LOGGER.info("Goodbye!");
-        System.exit(0);
-    }
 }
